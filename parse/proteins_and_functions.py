4a1b2fdb0cf5b358ddf5e9594f98c5955c063dce
djinx <key13043@gmail.com>
2019-07-18 23:37:21 +0200

    Izdvajanje funkcija i proteina koji ih vrse

--- parse/proteins_and_functions.py
+++ parse/proteins_and_functions.py
@@ -27,10 +27,35 @@ def proteins_with_functions(valid_proteins, path="../data/uniprot_sprot_exp.txt"
     return proteins
 
 
+def functions_with_proteins(path="../data/uniprot_sprot_exp.txt"):
+    # Funkcija za svaku funkciju odredjuje koji proteini je vrse
+    # Ima ih 5966, vecina su listovi ontologije, ali ima i unutrasnjih cvorova (1310)
+    file = open(path, "r")
+    all_lines = file.readlines()
+    functions = {}
+
+    for line in all_lines:
+        tokens = line.split(" ")
+
+        if tokens[2] == "F\n":
+            protein = tokens[0]
+            function = tokens[1]
+
+            if function in functions:
+                functions[function].append(protein)
+            else:
+                functions[function] = []
+
+    file.close()
+    return functions
+
+
 def main():
     valid_proteins = sequences.protein_sequences().keys()
     proteins = proteins_with_functions(valid_proteins)
+    functions = functions_with_proteins()
     print(len(proteins))
+    print(len(functions))
 
 
 if __name__ == '__main__':
