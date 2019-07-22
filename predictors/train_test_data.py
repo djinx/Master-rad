c11a2b3cf3ab7f5f84f00dffa1018c27566b13a0
djinx <key13043@gmail.com>
2019-07-22 23:11:56 +0200

    Funkcije za pripremu ulaznih datoteka

---
+++ predictors/train_test_data.py
@@ -0,0 +1,35 @@
+from sklearn import model_selection
+from parse import read_files
+
+
+def train_test(all_sequences, function):
+    positive_proteins = read_files.read_functions_with_proteins()[function]
+
+    x = []
+    y = []
+
+    for protein in all_sequences:
+        if protein in positive_proteins:
+            y.append(1)
+        else:
+            y.append(-1)
+
+        x.append(protein)
+
+    x_train_val, x_test, y_train_val, y_test = model_selection.train_test_split(x, y, test_size=0.25)
+    x_train, x_validation, y_train, y_validation = model_selection.train_test_split(x_train_val, y_train_val,
+                                                                                    test_size=0.25)
+
+    make_file(x_train, y_train, all_sequences, "../data/svm/train.txt")
+    make_file(x_validation, y_validation, all_sequences, "../data/svm/validation.txt")
+    make_file(x_test, y_test, all_sequences, "../data/svm/test.txt")
+
+
+def make_file(x, y, all_sequences, path):
+    file = open(path, "w")
+
+    for i in range(0, len(x)):
+        protein = x[i]
+        file.write(y[i] + " " + all_sequences[protein] + "\n")
+
+    file.close()
