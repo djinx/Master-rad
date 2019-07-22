3dd1548048e93efe320a98c620719df68a1587ca
djinx <key13043@gmail.com>
2019-07-22 13:47:50 +0200

    Citanje proteina sa funkcijama

--- parse/read_files.py
+++ parse/read_files.py
@@ -9,3 +9,20 @@ def read_alt_ids(path="../data/parsed_data/alt_ids.txt"):
 
     file.close()
     return alt_ids
+
+
+def read_proteins_with_functions(path="../data/parsed_data/proteins_with_functions.txt"):
+    file = open(path, "r")
+    lines = file.readlines()
+    proteins_with_functions = {}
+
+    for line in lines:
+        tokens = line.split("->")
+        protein = tokens[0]
+        functions = tokens[1].split(" ")
+        proteins_with_functions[protein] = []
+
+        for function in functions:
+            proteins_with_functions[protein].append(function)
+
+    return proteins_with_functions
