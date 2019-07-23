7c513a1311ec9decd5b75db9085667a6be1b7c40
djinx <key13043@gmail.com>
2019-07-23 22:34:05 +0200

    Treniranje modela

--- predictors/svm_light.py
+++ predictors/svm_light.py
@@ -2,28 +2,49 @@ from parse import read_files
 from predictors.train_test_data import train_test
 from datetime import datetime
 import subprocess
+import numpy as np
 
 
 def main():
+    # putanje
+    svm_learn = "../svm_light/svm_learn "
+    svm_classify = "../svm_light/svm_classify "
+    train = " ../data/svm/train.txt "
+    validation = " ../data/svm/validation.txt "
+    test = " ../data/svm/test.txt "
+
     # nekoliko proteina za testiranje klasifikatora
     all_sequences = read_files.read_array_sequences()
+    print(len(all_sequences))
 
     # funckija za koju se pravi klasifikator
-    function = "GO:0042802"
+    function = "GO:0004653"
 
     print("Priprema pozitivnih i negativnih instanci: ", datetime.now().time())
     train_test(all_sequences, function)
 
-    model = "../data/models/" + function[3:]
-    output = "../data/output/" + function[3:]
+    cs = [10**i for i in range(-3, 1)]
+    gammas = np.logspace(-5, 0, 6)
 
-    print("Pocelo trenuranje: ", datetime.now().time())
-    subprocess.run("../svm_light/svm_learn -m 200 -# 1000 ../data/svm/train.txt " + model)
-    print("Zavrseno treniranje: ", datetime.now().time())
+    for gamma in gammas:
+        for c in cs:
+            print(c, gamma)
+            model = " ../data/models/" + function[3:] + "_" + str(c) + "_" + str(gamma)
+            output = " ../data/output/" + function[3:] + "_" + str(c) + "_" + str(gamma)
+            model_terminal = "../data/terminal/train_" + function[3:] + "_" + str(c) + "_" + str(gamma) + ".txt"
+            output_terminal = "../data/terminal/test_ " + function[3:] + "_" + str(c) + "_" + str(gamma) + ".txt"
 
-    print("Pocelo testiranje: ", datetime.now().time())
-    subprocess.run("../svm_light/svm_classify ../data/svm/validation.txt " + model + " " + output)
-    print("Zavrseno testiranje: ", datetime.now().time())
+            parameters = "-c " + str(c) + " -t 3 -r " + str(gamma)
+
+            with open(model_terminal, "w") as file:
+                file.write("Pocelo trenuranje za c: " + str(c) + " " + str(datetime.now().time()) + "\n")
+                subprocess.run(svm_learn + parameters + train + model, stdout=file)
+                file.write("Zavrseno treniranje: " + str(datetime.now().time()) + "\n")
+
+            with open(output_terminal, "w") as file:
+                file.write("Pocelo testiranje: " + str(datetime.now().time()) + "\n")
+                subprocess.run(svm_classify + validation + model + output, stdout=file)
+                file.write("Zavrseno testiranje: " + str(datetime.now().time()) + "\n")
 
 
 if __name__ == '__main__':
