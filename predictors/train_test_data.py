0901ed6239060803098fe9be45e67ef7511d868c
djinx <key13043@gmail.com>
2019-07-25 19:35:05 +0200

    Funkcija za smanjivanje skupa

--- predictors/train_test_data.py
+++ predictors/train_test_data.py
@@ -1,15 +1,15 @@
 from sklearn import model_selection
 from parse import read_files
+import numpy as np
 
 
-def train_test(all_sequences, function):
+def train_test(all_sequences, function, size=20000):
     positive_proteins = read_files.read_functions_with_proteins()[function]
+    negative_proteins = read_files.read_proteins(positive_proteins)
 
-    # povecanje broja pozitivnih instanci
-    # dodaju se iste instance vise puta
-    total = len(all_sequences)
-    positive = len(positive_proteins)
-    n = (total - 2*positive) / positive
+    chosen_positives, chosen_negatives = smaller_set(positive_proteins, negative_proteins, len(all_sequences), size)
+    num_of_positives = len(chosen_positives)
+    n = (size / 2 - num_of_positives) / num_of_positives
 
     x = []
     y = []
@@ -42,3 +42,23 @@ def make_file(x, y, all_sequences, path):
         file.write(str(y[i]) + " " + all_sequences[protein] + "\n")
 
     file.close()
+
+
+def smaller_set(positives, negatives, num_of_proteins, n):
+    # Ukupan broj pozitivnih i negativnih instanci
+    total_num_of_positive = len(positives)
+    total_num_of_negative = num_of_proteins - total_num_of_positive
+
+    # Broj pozitivnih i negativnih instanci za uzorak
+    num_of_positive = round(len(positives) / num_of_proteins * n)
+    num_of_negative = n // 2
+
+    # Indeksi odabranih instanci
+    positive_indices = np.random.choice(total_num_of_positive, num_of_positive, replace=False)
+    negative_indices = np.random.choice(total_num_of_negative, num_of_negative, replace=False)
+
+    # Odabir instanci
+    chosen_positive = np.array(positives)[positive_indices]
+    chosen_negative = np.array(negatives)[negative_indices]
+
+    return chosen_positive, chosen_negative
