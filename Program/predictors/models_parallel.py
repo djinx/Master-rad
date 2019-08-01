4f4bf6ee4e624d4baab7624a9527dfdcde877b2f
djinx <key13043@gmail.com>
2019-08-01 16:49:39 +0200

    Sitna ispravka

--- Program/predictors/models_parallel.py
+++ Program/predictors/models_parallel.py
@@ -16,28 +16,28 @@ def all_models(function):
     gammas = np.logspace(-4, -1, 4)
 
     with multiprocessing.Pool(processes=4) as pool:
-        # pool.starmap(calculate_one_model, [(gamma, c, function) for gamma in gammas for c in cs])
-        pool.starmap(calculate_one_model, [(c, function) for c in cs])
+        pool.starmap(calculate_one_model, [(gamma, c, function) for gamma in gammas for c in cs])
+        # pool.starmap(calculate_one_model, [(c, function) for c in cs])
 
 
-# def calculate_one_model(gamma, c, function):
-def calculate_one_model(c, function):
-    print("Pocelo trenuranje za C=", c, datetime.now().time())
-    model = " ../data/models/" + function[3:] + "_c" + str(c)
-    output = " ../data/output/" + function[3:] + "_c" + str(c)
-    model_terminal = "../data/terminal/train_" + function[3:] + "_c" + str(c) + ".txt"
-    output_terminal = "../data/terminal/val_ " + function[3:] + "_c" + str(c) + ".txt"
+def calculate_one_model(gamma, c, function):
+    # def calculate_one_model(c, function):
+    print("Pocelo trenuranje za C=", c, "gamma=", gamma, datetime.now().time())
+    model = " ../data/models/rbf20k/" + function[3:] + "_g" + str(gamma) + "_c" + str(c)
+    output = " ../data/output/rbf20k/" + function[3:] + "_g" + str(gamma) + "_c" + str(c)
+    model_terminal = "../data/terminal/rbf20k/train_" + function[3:] + "_g" + str(gamma) + "_c" + str(c) + ".txt"
+    output_terminal = "../data/terminal/rbf20k/val_ " + function[3:] + "_g" + str(gamma) + "_c" + str(c) + ".txt"
 
-    parameters = "-c " + str(c)
+    parameters = "-c " + str(c) + " -t 2 -g " + str(gamma)
 
     with open(model_terminal, "w") as file:
-        file.write("Pocelo trenuranje za c: " + str(c) + " " + str(datetime.now().time()) + "\n")
+        file.write("Pocelo treniranje:" + str(datetime.now().time()) + "\n")
         subprocess.run(svm_learn + parameters + train + model, stdout=file)
-        file.write("Zavrseno treniranje: " + str(datetime.now().time()) + "\n")
+        file.write("Zavrseno treniranje:" + str(datetime.now().time()) + "\n")
 
     sleep(0.1)
 
     with open(output_terminal, "w") as file:
-        file.write("Pocelo testiranje: " + str(datetime.now().time()) + "\n")
+        file.write("Pocelo testiranje:" + str(datetime.now().time()) + "\n")
         subprocess.run(svm_classify + validation + model + output, stdout=file)
-        file.write("Zavrseno testiranje: " + str(datetime.now().time()) + "\n")
+        file.write("Zavrseno testiranje:" + str(datetime.now().time()) + "\n")
